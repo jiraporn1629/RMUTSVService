6679de6a65d33072d00fd2d09bab3000114ea8b5
jirapornApp <jira_1629_@hotmail.com>
2017-11-07 13:33:15 +0700

    Step1

--- app/src/main/java/jirapornmtrmutsv/rmutsvservice/fragment/MainFragment.java
+++ app/src/main/java/jirapornmtrmutsv/rmutsvservice/fragment/MainFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import jirapornmtrmutsv.rmutsvservice.R;
 
@@ -14,6 +15,26 @@ import jirapornmtrmutsv.rmutsvservice.R;
  */
 
 public class MainFragment extends Fragment{
+//    Manager Work after onCreateView Success
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+//        Rejister Controller
+        rejisterController();
+
+
+    } //Main Method
+
+    private void rejisterController() {
+        TextView textView = getView().findViewById(R.id.txtRegister);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
